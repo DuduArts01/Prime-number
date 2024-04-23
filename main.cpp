2ad06ef4284f103d0e2b3e7fb9c6ec2f54414f8f
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2024-04-23 06:24:18 -0300

    Atualizar o main.cpp

--- main.cpp
+++ main.cpp
@@ -39,21 +39,3 @@ int main()
     return 0;
 }
 
-/*
-n = int(input("Digite um número: "))
-
-numbers = []
-
-for i in range(2, n + 1):
-    if(i == 2):
-        numbers.append(i)
-    elif(i % 2 != 0):
-        count = 0
-        for c in range(1, i + 1):
-            if(i % c == 0):
-                count += 1 
-        if(count == 2):
-            numbers.append(i)
-            
-print(numbers)
-*/
