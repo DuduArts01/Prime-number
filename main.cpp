31799a591b84ffca9ccf7c57e479630c3f2bba43
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2024-04-23 12:10:12 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,13 +10,10 @@ int main()
     cout << "Enter a number: ";
     cin >> n;
     
-    int numbers[n];
-    
-    
+    cout << "\nResult of Prime Numbers:\n";
     for(int i = 2; i < n + 1; i++){
         if(i == 2){
-            numbers[mResult] = i;
-            ++mResult;
+            cout << i << "\n";
         } else if(i % 2 != 0){
             count = 0;
             for(int c = 1; c < i + 1; c++){
@@ -25,17 +22,10 @@ int main()
                 }
             }
             if(count == 2){
-                numbers[mResult] = i;
-                ++mResult;
+                cout << i << "\n";
             }
         }
     }
-    
-    cout << "\nResult of Prime Numbers:\n";
-    for(int m = 0; m < mResult; m++){
-        cout << numbers[m] << " ";
-    }
-    
+
     return 0;
 }
-
